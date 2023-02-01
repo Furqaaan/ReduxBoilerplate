d4f758c8fa7328bc9d6f145c4095c3967bb9bfaa
Furqan Hameed <furqancode@gmail.com>
2023-02-01 21:55:58 +0530

    Changed deprecated createStore to configureStore by redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,23 @@
+// Changed deprecated createStore to new ReduxToolkit configureStore
+// Redux toolkit has redux dev tool extension and combine reducers inbuilt and it automatically calls create store
+
+import countReducer from "./count/countReducer";
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+    reducer: {
+        count: countReducer,
+    },
+});
+
+export default store;
+
+
+/*
+
+OLDER VERSION
+-------------------
+
 // We need to create a store to access the state with redux
 
 import { createStore,applyMiddleware ,compose} from "redux";
@@ -10,4 +30,8 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
-export default store;
+export default store; 
+
+
+*/
+
